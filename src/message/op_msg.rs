404d5ec6f69461ba3f42cd6ff8d1bec90e169285
Felipe Coury <felipe.coury@gmail.com>
2023-03-03 15:21:17 -0300

    refact: improved how we write OP_MSG reply

--- src/message/op_msg.rs
+++ src/message/op_msg.rs
@@ -10,7 +10,6 @@ use tracing::{debug, error};
 use crate::{
     command::{
         buildinfo, error, getcmdlineopts, getparameter, hello, ismaster, ping, CommandError,
-        HEADER_SIZE,
     },
     OpMsgHandlingError,
 };
@@ -23,32 +22,53 @@ impl OpMsg {
 
         let msg = self.0;
         let docs = ser::to_vec(&doc)?;
-        let message_length = HEADER_SIZE + 5 + docs.len() as u32;
 
-        // header
-        stream.write_all(&message_length.to_le_bytes()).await?;
-        stream.write_all(&0u32.to_le_bytes()).await?; // request_id
-        stream
-            .write_all(&msg.header.request_id.to_le_bytes())
-            .await?; // response_to
-        stream.write_all(&1u32.to_le_bytes()).await?; // opcode - OP_REPLY = 1
-
-        // body
-        stream.write_all(&msg.flags.to_le_bytes()).await?; // flags
-
-        // documents
+        let mut buffer: Vec<u8> = Vec::new();
+        buffer.extend_from_slice(&0u32.to_le_bytes());
+        buffer.extend_from_slice(&mut msg.header.request_id.to_le_bytes());
+        buffer.extend_from_slice(&mut 1u32.to_le_bytes());
+        buffer.extend_from_slice(&mut msg.flags.to_le_bytes());
+        buffer.extend_from_slice(&mut msg.sections.get(0).unwrap().kind().to_le_bytes());
         let section = msg.sections.get(0).unwrap();
-        stream.write_u8(section.kind()).await?;
-
+        buffer.extend_from_slice(&mut section.kind().to_le_bytes());
         let bson_data: &[u8] = &docs;
-        stream.write_all(bson_data).await?;
+        buffer.extend_from_slice(bson_data);
+
+        debug!("len before: {:?}", buffer.len());
+        let message_length = buffer.len() as u32 + 4 as u32;
+        buffer.splice(..0, message_length.to_le_bytes().iter().cloned());
+        debug!("len after: {:?}", buffer.len());
 
         // TODO: checksum
 
+        stream.write_all(&buffer).await?;
+
         stream.flush().await?;
-        debug!("OP_MSG: [{cmd}] => {doc:?}", cmd = msg.command());
+        debug!(
+            "OP_MSG: [{cmd}] => ({size} - {message_length}) {doc:?}",
+            size = buffer.len(),
+            cmd = msg.command()
+        );
 
         Ok(())
+
+        // // header
+        // stream.write_all(&message_length.to_le_bytes()).await?; // 4
+        // stream.write_all(&0u32.to_le_bytes()).await?; // request_id (4)
+        // stream
+        //     .write_all(&msg.header.request_id.to_le_bytes())
+        //     .await?; // response_to (4)
+        // stream.write_all(&1u32.to_le_bytes()).await?; // opcode - OP_REPLY = 1 (4)
+
+        // // body
+        // stream.write_all(&msg.flags.to_le_bytes()).await?; // flags (4)
+
+        // // documents
+        // let section = msg.sections.get(0).unwrap();
+        // stream.write_u8(section.kind()).await?;
+
+        // let bson_data: &[u8] = &docs;
+        // stream.write_all(bson_data).await?;
     }
 
     async fn run(&self) -> anyhow::Result<Document> {
