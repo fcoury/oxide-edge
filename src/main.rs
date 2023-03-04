5a1c3f2519f678baf099879ed2a8fd3d2dc88025
Felipe Coury <felipe.coury@gmail.com>
2023-03-04 11:01:28 -0300

    refact: extracted Server and Request

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use std::error::Error;
 
 use clap::Parser;
-use futures::FutureExt;
-use mongodb_wire_protocol_parser::{parse, OpCode};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
-use tracing::{instrument, trace};
+use server::Server;
 
 use crate::cli::Cli;
-use crate::log::log;
-use crate::message::{OpMsg, OpQuery, OpReply};
 
 mod cli;
 mod command;
@@ -17,115 +11,13 @@ mod error;
 mod log;
 mod message;
 mod query;
+mod server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt::init();
     let cli = Cli::parse();
-    let listen_addr = &cli.listen;
 
-    let listener = TcpListener::bind(listen_addr).await?;
-    tracing::info!("server listening on {}", listen_addr);
-
-    if let Some(proxies) = &cli.proxy {
-        tracing::info!("proxying to: {proxies:?}");
-    }
-
-    let mut id = 0;
-    while let Ok((inbound, _)) = listener.accept().await {
-        tracing::debug!("accepted connection from: {}", inbound.peer_addr()?);
-        id += 1;
-        let handler = handle(id, inbound, cli.clone()).map(|r| {
-            if let Err(e) = r {
-                tracing::error!("error: {}", e);
-            }
-        });
-
-        tokio::spawn(handler);
-    }
-
-    Ok(())
-}
-
-#[instrument(
-    name = "handle",
-    skip(main_id, inbound, cli),
-    fields(id = %main_id),
-)]
-async fn handle(main_id: i32, mut inbound: TcpStream, cli: Cli) -> Result<(), Box<dyn Error>> {
-    let mut local_id = 0;
-    loop {
-        local_id += 1;
-
-        let id = format!("{main_id}-{local_id}");
-        let mut data = Vec::new();
-
-        let mut buf = [0; 4];
-        let n = inbound.peek(&mut buf).await?;
-        if n == 0 {
-            break;
-        }
-        let size: u32 = u32::from_le_bytes(buf);
-        trace!("SIZE = {}", size);
-        if size == 0 {
-            break;
-        }
-
-        loop {
-            let mut buf = [0; 4096];
-            let n = inbound.read(&mut buf).await?;
-            if n == 0 {
-                break;
-            }
-            data.extend_from_slice(&buf[..n]);
-            if n < buf.len() {
-                break;
-            }
-        }
-
-        // send data to proxies
-        if let Some(proxies) = &cli.proxy {
-            for (i, proxy) in proxies.iter().enumerate() {
-                let mut proxy = TcpStream::connect(proxy).await?;
-                proxy.write_all(&data).await?;
-                proxy.flush().await?;
-
-                let mut buf = [0; 4];
-                proxy.peek(&mut buf).await?;
-                let size: u32 = u32::from_le_bytes(buf);
-                trace!("proxy {i} size = {size}");
-
-                let mut proxy_data = vec![0; size as usize];
-                proxy.read_exact(&mut proxy_data).await?;
-                trace!("proxy {i} data = {:?}", proxy_data);
-
-                let msg = OpReply::parse(&proxy_data)?;
-                let json = serde_json::to_string_pretty(&msg.documents())?;
-
-                log(&id, "bin", "request", &data).await;
-                log(&id, "bin", format!("response-{i}"), &proxy_data).await;
-                log(
-                    &id,
-                    "txt",
-                    format!("response-{i}"),
-                    format!("{msg:#?}").as_bytes(),
-                )
-                .await;
-                log(&id, "json", format!("response-{i}"), json.as_bytes()).await;
-            }
-        }
-
-        trace!("DATA = {data:?}");
-        let msg = parse(data)?;
-        trace!("MSG = {msg:?}");
-
-        log(&id, "txt", "request", format!("{msg:#?}").as_bytes()).await;
-
-        match msg {
-            OpCode::OpMsg(msg) => OpMsg(msg).handle(&id, &mut inbound).await?,
-            OpCode::OpQuery(query) => OpQuery(query).handle(&id, &mut inbound).await?,
-        };
-    }
-
-    Ok(())
+    let server = Server::new(cli);
+    server.start().await
 }
---
+++ src/server.rs
@@ -0,0 +1,154 @@
+use std::error::Error;
+
+use mongodb_wire_protocol_parser::{parse, OpCode};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tracing::{instrument, trace};
+
+use crate::cli::Cli;
+use crate::log::log;
+use crate::message::{OpMsg, OpQuery, OpReply};
+
+#[derive(Debug, Clone)]
+pub struct Server {
+    cli: Cli,
+}
+
+#[derive(Debug, Clone)]
+pub struct Request {
+    id: i32,
+    cli: Cli,
+}
+
+impl Server {
+    pub fn new(cli: Cli) -> Self {
+        Self { cli }
+    }
+
+    pub async fn start(self) -> Result<(), Box<dyn Error>> {
+        let listener = TcpListener::bind(&self.cli.listen).await?;
+        tracing::info!("server listening on {}", &self.cli.listen);
+
+        if let Some(proxies) = &self.cli.proxy {
+            tracing::info!("proxying to: {proxies:?}");
+        }
+
+        let mut id = 0;
+        while let Ok((inbound, _)) = listener.accept().await {
+            tracing::debug!("accepted connection from: {}", inbound.peer_addr()?);
+            id += 1;
+
+            let handler = {
+                let request = Request::new(id, self.cli.clone());
+                async move {
+                    let result = request.handle(inbound).await;
+                    if let Err(e) = result {
+                        tracing::error!("error: {e}");
+                    }
+                }
+            };
+
+            tokio::spawn(handler);
+        }
+
+        Ok(())
+    }
+}
+
+impl Request {
+    pub fn new(id: i32, cli: Cli) -> Self {
+        Self { id, cli }
+    }
+
+    #[instrument(
+        name = "handle",
+        skip(inbound),
+        fields(id = %self.id),
+    )]
+    async fn handle(&self, mut inbound: TcpStream) -> Result<(), Box<dyn Error>> {
+        let main_id = self.id;
+        let mut local_id = 0;
+        loop {
+            local_id += 1;
+
+            let id = format!("{main_id}-{local_id}");
+            let mut data = Vec::new();
+
+            let mut buf = [0; 4];
+            let n = inbound.peek(&mut buf).await?;
+            if n == 0 {
+                break;
+            }
+            let size: u32 = u32::from_le_bytes(buf);
+            trace!("SIZE = {}", size);
+            if size == 0 {
+                break;
+            }
+
+            loop {
+                let mut buf = [0; 4096];
+                let n = inbound.read(&mut buf).await?;
+                if n == 0 {
+                    break;
+                }
+                data.extend_from_slice(&buf[..n]);
+                if n < buf.len() {
+                    break;
+                }
+            }
+
+            // send data to proxies
+            self.send_to_proxies(&id, &data).await?;
+
+            trace!("DATA = {data:?}");
+            let msg = parse(data)?;
+            trace!("MSG = {msg:?}");
+
+            log(&id, "txt", "request", format!("{msg:#?}").as_bytes()).await;
+
+            match msg {
+                OpCode::OpMsg(msg) => OpMsg(msg).handle(&id, &mut inbound).await?,
+                OpCode::OpQuery(query) => OpQuery(query).handle(&id, &mut inbound).await?,
+            };
+        }
+
+        Ok(())
+    }
+
+    async fn send_to_proxies(&self, id: &str, data: &[u8]) -> Result<(), Box<dyn Error>> {
+        let Some(proxies) = &self.cli.proxy else {
+            return Ok(());
+        };
+
+        for (i, proxy) in proxies.iter().enumerate() {
+            let mut proxy = TcpStream::connect(proxy).await?;
+            proxy.write_all(&data).await?;
+            proxy.flush().await?;
+
+            let mut buf = [0; 4];
+            proxy.peek(&mut buf).await?;
+            let size: u32 = u32::from_le_bytes(buf);
+            trace!("proxy {i} size = {size}");
+
+            let mut proxy_data = vec![0; size as usize];
+            proxy.read_exact(&mut proxy_data).await?;
+            trace!("proxy {i} data = {:?}", proxy_data);
+
+            let msg = OpReply::parse(&proxy_data)?;
+            let json = serde_json::to_string_pretty(&msg.documents())?;
+
+            log(id, "bin", "request", &data).await;
+            log(id, "bin", format!("response-{i}"), &proxy_data).await;
+            log(
+                id,
+                "txt",
+                format!("response-{i}"),
+                format!("{msg:#?}").as_bytes(),
+            )
+            .await;
+            log(id, "json", format!("response-{i}"), json.as_bytes()).await;
+        }
+
+        Ok(())
+    }
+}
